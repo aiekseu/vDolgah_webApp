1a68e46aa31833f933643500e4e1bab2e4cb7fe6
pi4alb <kudashkin_aleksey@mail.ru>
2020-09-29 00:41:41 +0700

    Updates

---
+++ precache-manifest.63c11cc6e9b24ce317a087bbc44b6bd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72b3bb60fef4153a0179fa9fef2c8aa",
+    "url": "/vDolgah_webApp/index.html"
+  },
+  {
+    "revision": "fc79f5892a7d4f16283d",
+    "url": "/vDolgah_webApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "78436f7063a5f86ac236",
+    "url": "/vDolgah_webApp/static/js/2.6d16000d.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/vDolgah_webApp/static/js/2.6d16000d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc79f5892a7d4f16283d",
+    "url": "/vDolgah_webApp/static/js/main.a2ce4b71.chunk.js"
+  },
+  {
+    "revision": "cc0b5835fb80a152c9e1",
+    "url": "/vDolgah_webApp/static/js/runtime-main.82290985.js"
+  }
+]);
