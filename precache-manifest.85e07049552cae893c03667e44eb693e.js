ecb91174dede8f193f70df7438814360647abbe7
pi4alb <kudashkin_aleksey@mail.ru>
2020-09-30 09:28:40 +0700

    Updates

---
+++ precache-manifest.85e07049552cae893c03667e44eb693e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bfa6f46b0e5d5cf7142a6fcfcea061",
+    "url": "/vDolgah_webApp/index.html"
+  },
+  {
+    "revision": "b7c86be4eb49c7eec18c",
+    "url": "/vDolgah_webApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "42591a307dea3fe30991",
+    "url": "/vDolgah_webApp/static/js/2.64ce897d.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/vDolgah_webApp/static/js/2.64ce897d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c86be4eb49c7eec18c",
+    "url": "/vDolgah_webApp/static/js/main.c1a1df68.chunk.js"
+  },
+  {
+    "revision": "cc0b5835fb80a152c9e1",
+    "url": "/vDolgah_webApp/static/js/runtime-main.82290985.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57f798ab800fb3397ecb5c737539fa97.js"
+  "/vDolgah_webApp/precache-manifest.85e07049552cae893c03667e44eb693e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vDolgah_webApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
